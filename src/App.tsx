88f283efbee1462acd623f6c35dc09103656a7a6
Alex <belohvostov.belohvostov@gmail.com>
2022-12-02 13:48:11 +0300

    init proj, install dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-import "./App.css";
-
-function App() {
-  return <div className="App">hello world</div>;
-}
-
-export default App;
+export const App: React.FC = () => {
+  return <div>Hello world</div>;
+};
